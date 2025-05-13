6dcdb39943b7da3ea1281c8d1f0a949c8efce539
pepedanado <futebolcolacotcc@gmail.com>
2025-05-13 17:44:17 -0300

    Primeira programação de Pynthon
    Uma pequena descrição de um exemplo de Pynthon.

---
+++ primeiro-programa.py
@@ -0,0 +1,28 @@
+
+a=4
+b=10
+c=5.0
+d=1
+f=5
+print(a==c)
+print(a<b)
+print(d>b)
+print(c!=f)
+print(a==b)
+print(c<d)
+print(b>a)
+print(c>=f)
+print(f>=c)
+print(c<=c)
+print(c<=f)
+p ="pedro henrique" 
+print(p[7:5])
+print(p[9])
+
+print("olá mundo")
+print(len("paralelepipedo"))
+print("chute"in"chuteira")
+print("soccer".upper())
+print("talco".replace("t","p"))
+y="powerade"
+print(y.startswith("power"))
