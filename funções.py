d24a086347b8f469af967c52dc5e1e5f7f381dad
pepedanado <futebolcolacotcc@gmail.com>
2025-05-15 16:48:05 -0300

    funções_py
    Um pouco sobre funções...

---
+++ funções.py
@@ -0,0 +1,61 @@
+"""texto_escrito="este livro é legal!"
+outro_texto="vou aprender a programar"
+outro_texto=texto_escrito
+print(outro_texto)
+balas_subtraidas=3
+quantidade_balas=20
+quantidade_balas-=balas_subtraidas
+print(quantidade_balas)
+nome=input()
+saudacao="olá"+nome
+print(saudacao)
+numero_um=input()
+numero_dois=input()
+resultado_soma=float(numero_um)+float(numero_dois)
+resultado_soma_texto=str(resultado_soma)
+mensagem="O resultado da soma é:"+resultado_soma_texto
+print(mensagem)"""
+
+def quadrado(lado):
+    area=lado*lado
+    print(area)
+quadrado(int(input()))
+def reajuste(salario):
+    novo_salario=salario*+15/100
+    print(novo_salario)
+reajuste(int(input()))
+
+
+
+def triangulo():
+    b=input()
+    h=input()
+    area=(int(b)*int(h))/2
+    A=area
+    print(A)
+
+triangulo()
+
+def celsius():
+    temperatura=int(input("digite sua temperatura"))
+    F=(9*temperatura+160)/5
+
+    print(F)
+celsius()
+
+
+    
+    
+
+
+
+
+
+    
+
+   
+    
+    
+    
+
+
