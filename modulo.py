5b8ef5e406705f907dad8e1d023c7b5d1386aba4
Pedro Mendes <futebolcolacotcc@gmail.com>
2025-05-21 17:24:56 -0300

    Modulo_py
    Um pouco sobre modelo e uma demonstração de random com times do brasileirão.

---
+++ modulo.py
@@ -0,0 +1,16 @@
+import random
+
+numero_aleatorio = random.random()
+#print(numero_aleatorio)
+
+import random 
+
+numero_aleatorio = random.randint(0,10)
+#print(numero_aleatorio)
+
+import random
+
+brasileirao = ['santos', 'gremio', 'inter', 'corinthians', 'palmeiras', 'sao paulo', 'sport', 'ceara', 'fortaleza', 'flamengo', 'fluminense', 'botafogo', 'vasco', 'bahia']
+print(random.choice(brasileirao), " x ", random.choice(brasileirao))
+
+
