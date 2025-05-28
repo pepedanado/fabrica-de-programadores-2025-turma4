90e4604a6a3d1aecee2274a501f66ff326493e39
Pedro Mendes <futebolcolacotcc@gmail.com>
2025-05-28 17:31:59 -0300

    While_e_Break.py
    Um pouco de programação usando while e break...

--- while.py
+++ while.py
@@ -1,4 +1,4 @@
-x = 1
+"""x = 1
 while x<=100:
     print(x)
     x = x+1
@@ -32,4 +32,40 @@ n = int(input("tabuada de:"))
 x = 1
 while x<=100:
     print(n*x)
+    x=x+1
+
+n = int(input("tabuada de: "))
+y = 1
+conta = n 
+while y<=10:
+    print(conta, "x" ,y ,"=", n*y)
+    y=y+1
+
+n = int(input("tabuada de: "))
+y = int(input("valor y, inicio da tabuada: "))
+z = int(input("fim da tabuada: "))
+conta = n 
+while y<=z:
+    print(conta, "x" ,y ,"=", n*y)
+    y=y+1
+
+s = 0
+while True:
+    v = int(input("Digite um numero a somar ou 0 para sair: "))
+    if v==0:
+        break
+    s = s+v
+print(s)
+
+lista = []
+while True:
+    n = (input("Digite um carro: "))
+    if n=="bmw":
+        break
+    lista.append(n)
+print(lista)"""
+
+x = 0
+while x < len(lista):
+    print(lista[x])
     x=x+1
